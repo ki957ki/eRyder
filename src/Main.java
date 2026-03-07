131405cd59a5c25eb3a416a7c6852dcf28f95595
Meiqi Jiang <2249831949@qq.com>
2026-03-07 13:00:48 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        eRyder bike = new eRyder("Bike001",80,true,15.5);
+        bike.printBikeDetails();
+        bike.ride(80, true);}
+
+}
